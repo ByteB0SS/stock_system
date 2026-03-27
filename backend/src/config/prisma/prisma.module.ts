937014653f9a85644909e7fc823949222b982566
Rubem Erensto Figueiredo <rubemernesto2@gmail.com>
2026-03-27 12:27:36 +0100

    fix: prisma error to prisma work with docker

--- backend/src/config/prisma/prisma.module.ts
+++ backend/src/config/prisma/prisma.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from './prisma.service'
 
-@Global() 
+
+@Global()
 @Module({
   providers: [PrismaService],
-  exports: [PrismaService], 
+  exports: [PrismaService]
 })
 export class PrismaModule {}
--- backend/src/config/prisma/prisma.service.ts
+++ backend/src/config/prisma/prisma.service.ts
@@ -1,19 +1,8 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-
-// import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    super({
-      adapter: new PrismaPg({
-        connectionString: process.env.DATABASE_URL, 
-      }),
-    });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
